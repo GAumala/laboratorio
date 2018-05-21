1ab7ee99d6e4f114fc06c5f8f8e69068b6217f1b
Gabriel Aumala <gabrielfima@live.com>
2018-05-21 11:39:36 -0500

    Finish UI for HemogramaView

---
+++ frontend/src/Model/HemogramaIDs.elm
@@ -0,0 +1,85 @@
+module Model.HemogramaIDs exposing (..)
+
+
+eritrocitos =
+    "hemograma-eritrocitos-input"
+
+
+hematocrito =
+    "hemograma-hematocrito-input"
+
+
+hemoglobina =
+    "hemograma-hemoglobina-input"
+
+
+eritroblastos =
+    "hemograma-eritroblastos-input"
+
+
+reticulocitos =
+    "hemograma-reticulocitos-input"
+
+
+vcm =
+    "hemograma-vcm-input"
+
+
+hcm =
+    "hemograma-hcm-input"
+
+
+mchc =
+    "hemograma-mchc-input"
+
+
+plaquetas =
+    "hemograma-plaquetas-input"
+
+
+mpv =
+    "hemograma-mpv-input"
+
+
+pdw =
+    "hemograma-pdw-input"
+
+
+pct =
+    "hemograma-pct-input"
+
+
+eritrosedimentacion =
+    "hemograma-eritrosedimentacion-input"
+
+
+leucocitos =
+    "hemograma-leucocitos-input"
+
+
+juveniles =
+    "hemograma-juveniles-input"
+
+
+cayados =
+    "hemograma-cayados-input"
+
+
+segmentados =
+    "hemograma-segmentados-input"
+
+
+eosinofilos =
+    "hemograma-eosinofilos-input"
+
+
+basofilos =
+    "hemograma-basofilos-input"
+
+
+monocitos =
+    "hemograma-monocitos-input"
+
+
+linfocitos =
+    "hemograma-linfocitos-input"
--- frontend/src/View/HemogramaView.elm
+++ frontend/src/View/HemogramaView.elm
@@ -10,9 +10,10 @@ import Model
             )
         , TextFieldState
         )
+import Model.HemogramaIDs as IDs
 import Model.FormModel as FormModel
-import Html exposing (Html, div, h1, span, text, table, tr, td)
-import Html.Attributes exposing (class, style)
+import Html exposing (Html, div, h1, h3, span, text, table, tr, td)
+import Html.Attributes exposing (colspan, class, style)
 import View.MDL as MDL
 
 
@@ -26,6 +27,24 @@ formRow columns =
         [ td [ class "name-label-col" ] [ columns.nameLabel ]
         , td [ class "input-field-col" ] [ columns.inputField ]
         , td [ class "unit-label-col" ] [ columns.unitLabel ]
+        ]
+
+
+sectionHeaderRow : String -> Html msg
+sectionHeaderRow sectionName =
+    tr []
+        [ td [ class "section-header", colspan 3 ]
+            [ span [] [ text sectionName ]
+            ]
+        ]
+
+
+submitButtonRow : Html Msg
+submitButtonRow =
+    tr []
+        [ td [ class "submit-button-row", colspan 3 ]
+            [ MDL.mainButton { text = "Guardar", onClick = NoOp }
+            ]
         ]
 
 
@@ -43,37 +62,293 @@ numericInputConfig id state =
     }
 
 
+normalFormRow : { inputId : String, state : TextFieldState, name : String, unit : String } -> Html Msg
+normalFormRow { inputId, state, name, unit } =
+    formRow
+        { nameLabel = span [ class "form-text" ] [ text name ]
+        , inputField =
+            MDL.textField <| numericInputConfig inputId state
+        , unitLabel = span [ class "form-text" ] [ text unit ]
+        }
+
+
 eritrocitosRow : HemogramaModel -> Html Msg
 eritrocitosRow model =
     let
-        textFieldId =
-            "hemograma-eritrocitos-input"
-
-        state =
-            FormModel.get textFieldId model
+        inputId =
+            IDs.eritrocitos
     in
-        formRow
-            { nameLabel = span [ class "form-text" ] [ text "eritrocitos" ]
-            , inputField =
-                MDL.textField <| numericInputConfig textFieldId state
-            , unitLabel = span [ class "form-text" ] [ text "x 10 6/UL" ]
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Eritrocitos"
+            , unit = "x 10 6/UL"
             }
 
 
 hematocritoRow : HemogramaModel -> Html Msg
 hematocritoRow model =
     let
-        textFieldId =
-            "hemograma-hematocrito-input"
-
-        state =
-            FormModel.get textFieldId model
+        inputId =
+            IDs.hematocrito
     in
-        formRow
-            { nameLabel = span [ class "form-text" ] [ text "hematocrito" ]
-            , inputField =
-                MDL.textField <| numericInputConfig textFieldId state
-            , unitLabel = span [ class "form-text" ] [ text "%" ]
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Hematócrito"
+            , unit = "%"
+            }
+
+
+hemoglobinaRow : HemogramaModel -> Html Msg
+hemoglobinaRow model =
+    let
+        inputId =
+            IDs.hemoglobina
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Hemoglobina"
+            , unit = "g/dL"
+            }
+
+
+eritroblastosRow : HemogramaModel -> Html Msg
+eritroblastosRow model =
+    let
+        inputId =
+            IDs.eritroblastos
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Eritroblastos"
+            , unit = "%"
+            }
+
+
+reticulocitosRow : HemogramaModel -> Html Msg
+reticulocitosRow model =
+    let
+        inputId =
+            IDs.reticulocitos
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Reticulocitos"
+            , unit = "%"
+            }
+
+
+vcmRow : HemogramaModel -> Html Msg
+vcmRow model =
+    let
+        inputId =
+            IDs.vcm
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "VCM"
+            , unit = "fL"
+            }
+
+
+hcmRow : HemogramaModel -> Html Msg
+hcmRow model =
+    let
+        inputId =
+            IDs.hcm
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "HCM"
+            , unit = "pg"
+            }
+
+
+mchcRow : HemogramaModel -> Html Msg
+mchcRow model =
+    let
+        inputId =
+            IDs.mchc
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "MCHC"
+            , unit = "pg"
+            }
+
+
+plaquetasRow : HemogramaModel -> Html Msg
+plaquetasRow model =
+    let
+        inputId =
+            IDs.plaquetas
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Plaquetas"
+            , unit = "x 10 3/uL"
+            }
+
+
+mpvRow : HemogramaModel -> Html Msg
+mpvRow model =
+    let
+        inputId =
+            IDs.mpv
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "MPV"
+            , unit = "fL"
+            }
+
+
+pdwRow : HemogramaModel -> Html Msg
+pdwRow model =
+    let
+        inputId =
+            IDs.pdw
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "PDW"
+            , unit = "%"
+            }
+
+
+pctRow : HemogramaModel -> Html Msg
+pctRow model =
+    let
+        inputId =
+            IDs.pct
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "PCT"
+            , unit = "%"
+            }
+
+
+leucocitosRow : HemogramaModel -> Html Msg
+leucocitosRow model =
+    let
+        inputId =
+            IDs.leucocitos
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Contaje leucocitos"
+            , unit = "x 10 3/uL"
+            }
+
+
+juvenilesRow : HemogramaModel -> Html Msg
+juvenilesRow model =
+    let
+        inputId =
+            IDs.juveniles
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Juveniles"
+            , unit = "%"
+            }
+
+
+cayadosRow : HemogramaModel -> Html Msg
+cayadosRow model =
+    let
+        inputId =
+            IDs.cayados
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Cayados"
+            , unit = "%"
+            }
+
+
+segmentadosRow : HemogramaModel -> Html Msg
+segmentadosRow model =
+    let
+        inputId =
+            IDs.segmentados
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Segmentados"
+            , unit = "%"
+            }
+
+
+eosinofilosRow : HemogramaModel -> Html Msg
+eosinofilosRow model =
+    let
+        inputId =
+            IDs.eosinofilos
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Eosinófilos"
+            , unit = "%"
+            }
+
+
+basofilosRow : HemogramaModel -> Html Msg
+basofilosRow model =
+    let
+        inputId =
+            IDs.basofilos
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Basófilos"
+            , unit = "%"
+            }
+
+
+monocitosRow : HemogramaModel -> Html Msg
+monocitosRow model =
+    let
+        inputId =
+            IDs.monocitos
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Monocitos"
+            , unit = "%"
+            }
+
+
+linfocitosRow : HemogramaModel -> Html Msg
+linfocitosRow model =
+    let
+        inputId =
+            IDs.linfocitos
+    in
+        normalFormRow
+            { inputId = inputId
+            , state = FormModel.get inputId model
+            , name = "Linfocitos"
+            , unit = "%"
             }
 
 
@@ -82,7 +357,28 @@ view model =
     div []
         [ h1 [] [ text "Hemograma" ]
         , table [ class "center" ]
-            [ eritrocitosRow model
+            [ sectionHeaderRow "Eritrograma"
+            , eritrocitosRow model
             , hematocritoRow model
+            , hemoglobinaRow model
+            , eritroblastosRow model
+            , reticulocitosRow model
+            , vcmRow model
+            , hcmRow model
+            , mchcRow model
+            , plaquetasRow model
+            , mpvRow model
+            , pdwRow model
+            , pctRow model
+            , sectionHeaderRow "Leucograma"
+            , leucocitosRow model
+            , juvenilesRow model
+            , cayadosRow model
+            , segmentadosRow model
+            , eosinofilosRow model
+            , basofilosRow model
+            , monocitosRow model
+            , linfocitosRow model
+            , submitButtonRow
             ]
         ]
